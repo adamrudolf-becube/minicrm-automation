46ce2e8a04eee4d874f50c17f7ecda6348f7ec11
BeCube <jelentkezem@becube.hu>
2019-01-16 16:37:57 +0100

    Add folder for tests with (currentl) experimental files

---
+++ test/users.py
@@ -0,0 +1,14 @@
+# users.py
+
+import requests
+
+USERS_URL = 'http://jsonplaceholder.typicode.com/users'
+
+
+def get_users():
+    """Get list of users"""
+    response = requests.get(USERS_URL)
+    if response.ok:
+        return response
+    else:
+        return None

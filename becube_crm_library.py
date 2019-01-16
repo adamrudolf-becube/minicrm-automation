0651f2d443bb7add8afdd06536bdab084773564c
BeCube <jelentkezem@becube.hu>
2019-01-16 08:28:16 +0100

    Refactor utility functions to 'utility_functions.py'

--- becube_crm_library.py
+++ becube_crm_library.py
@@ -8,67 +8,17 @@ import sys
 import json
 import subprocess
 import shlex
-import functools
-import traceback
 import datetime
+from utility_functions import stacktrace, trace, pretty_print
 
 API_INFO_JSON_FILE = "api_info.json"
 
-INDENT = 4*' '
-INDENT_GENERAL = 0
-
-TRACING = True
 
 system_id = None
 api_key = None
 
 reload(sys)
-sys.setdefaultencoding('utf8')   
-
-
-def indent(txt):
-    global INDENT_GENERAL
-    return '\n'.join(" " * INDENT_GENERAL + line for line in  txt.splitlines())
-
-
-def stacktrace(func):
-    @functools.wraps(func)
-    def wrapped(*args, **kwds):
-        # Get all but last line returned by traceback.format_stack()
-        # which is the line below.'
-        global INDENT_GENERAL
-        INDENT_GENERAL += 4
-        callstack = '\n'.join([INDENT+line.strip() for line in traceback.format_stack()][:-1])
-        if TRACING:
-            print('\n' + indent('-'*len('{}() called:'.format(func.__name__))))
-            print(indent('{}() called:'.format(func.__name__)))
-            print(indent(callstack))
-        return_value = func(*args, **kwds)
-        INDENT_GENERAL -= 4
-        return return_value
-
-    return wrapped    
-
-
-def trace(message):
-    if TRACING:
-        print()
-        print(indent(message))
-        print()
-    pass
-
-def format_json(json_array):
-    """
-    Returns a human-readable JSON array
-    """
-    return json.dumps(json_array, sort_keys=True, indent=4, separators=(',', ': '))
-
-def pretty_print(json_array):
-    """
-    Prints a formatted JSON array
-    """
-    print(indent(format_json(json_array)))
-    pass
+sys.setdefaultencoding('utf8')
 
 
 def load_api_info():
---
+++ utility_functions.py
@@ -0,0 +1,52 @@
+import functools
+import traceback
+import json
+
+INDENT = 4*' '
+INDENT_GENERAL = 0
+
+TRACING = True
+
+def indent(txt):
+    global INDENT_GENERAL
+    return '\n'.join(" " * INDENT_GENERAL + line for line in  txt.splitlines())
+
+
+def stacktrace(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwds):
+        # Get all but last line returned by traceback.format_stack()
+        # which is the line below.'
+        global INDENT_GENERAL
+        INDENT_GENERAL += 4
+        callstack = '\n'.join([INDENT+line.strip() for line in traceback.format_stack()][:-1])
+        if TRACING:
+            print('\n' + indent('-'*len('{}() called:'.format(func.__name__))))
+            print(indent('{}() called:'.format(func.__name__)))
+            print(indent(callstack))
+        return_value = func(*args, **kwds)
+        INDENT_GENERAL -= 4
+        return return_value
+
+    return wrapped
+
+
+def trace(message):
+    if TRACING:
+        print()
+        print(indent(message))
+        print()
+    pass
+
+def format_json(json_array):
+    """
+    Returns a human-readable JSON array
+    """
+    return json.dumps(json_array, sort_keys=True, indent=4, separators=(',', ': '))
+
+def pretty_print(json_array):
+    """
+    Prints a formatted JSON array
+    """
+    print(indent(format_json(json_array)))
+    pass

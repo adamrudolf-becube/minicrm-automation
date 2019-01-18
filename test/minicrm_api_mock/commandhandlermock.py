b9a38775ed2d3f34029c11aa40eecdf58691d9d7
BeCube <jelentkezem@becube.hu>
2019-01-18 17:07:14 +0100

    Mock API reads answer from textfile

--- test/minicrm_api_mock/commandhandlermock.py
+++ test/minicrm_api_mock/commandhandlermock.py
@@ -5,8 +5,20 @@ class CommandHandlerMock:
 
     def __init__(self):
         self.commands_got_in_parameters = []
+        self.return_values = {}
 
     @stacktrace
     def get_json_array_for_command(self, command):
         print("COMMAND SENT TO ---MOCK--- API: {}".format(command))
         self.commands_got_in_parameters.append(command)
+        return self.read_file(self.return_values[command])
+
+    @stacktrace
+    def set_return_value(self, command, filename):
+        self.return_values[command] = filename
+
+    @stacktrace
+    def read_file(self, filename):
+        with open(filename, 'r') as inputfile:
+            contents = inputfile.read()
+        return contents
--- test/test_quickscript.py
+++ test/test_quickscript.py
@@ -1,20 +1,18 @@
 from becube_crm_library import *
 from test.minicrm_api_mock.commandhandlermock import CommandHandlerMock
 
-API_INFO_JSON_FILE = "../api_info.json"
-
-system_id = None
-api_key = None
+API_INFO_JSON_FILE = "../api_info_real.json"
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-def quick_script():
-    global system_id
-    global api_key
-
+def quick_script(system_id, api_key):
     command_handler = CommandHandlerMock()
+    command_handler.set_return_value(
+        'curl -s --user 38539:xhkjtSKq6v40BdfnsGIwrWLQ7OFiEDab "https://r3.minicrm.hu/Api/R3/Category"',
+        'api_outputs/category_01.txt'
+    )
     crm_data = CrmData(system_id, api_key, command_handler)
     crm_data.clean_info_level_kiment()
     crm_data.handle_waiting_list()
@@ -23,5 +21,5 @@ def quick_script():
 
 
 if __name__ == "__main__":
-    load_api_info(API_INFO_JSON_FILE)
-    quick_script()
+    system_id, api_key = load_api_info(API_INFO_JSON_FILE)
+    quick_script(system_id, api_key)

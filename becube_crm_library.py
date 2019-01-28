cb0b54f7c3aed99cb02e2535251f4af9ab147761
BeCube <jelentkezem@becube.hu>
2019-01-28 07:12:00 +0100

    Add test of quickscript

--- becube_crm_library.py
+++ becube_crm_library.py
@@ -58,9 +58,10 @@ class Module:
     Stores a collection of projects, and other module-level data, for example status codes
     """
     @stacktrace
-    def __init__(self, module_id, system_id, api_key):
+    def __init__(self, module_id, system_id, api_key, command_handler):
         self.module_id = module_id
         self.api_key = api_key
+        self.command_handler = command_handler
         self.system_id = system_id
         self.available_values = self.query_available_values()
         self.project_list = self.query_project_list()
@@ -161,7 +162,7 @@ class CrmData:
     #                                                                         #
     ###########################################################################
     @stacktrace
-    def __init__(self, system_id, api_key, command_handler):
+    def __init__(self, system_id, api_key, command_handler, today=datetime.datetime.now()):
         """
         Sets the login data required by the API, collects information about existing modules, and even initializes some fo them
         """
@@ -169,8 +170,17 @@ class CrmData:
         self.system_id = system_id
         self.command_handler = command_handler
         self.set_modules_dictionary()
-        self.jelentkezok = CustomerList(self.get_module_number_by_name("Jelentkezés"), self.system_id, self.api_key)
-        self.tanfolymok = CourseList(self.get_module_number_by_name("Tanfolyamok"), self.system_id, self.api_key)
+        self.jelentkezok = CustomerList(
+            self.get_module_number_by_name("Jelentkezés"),
+            self.system_id,
+            self.api_key,
+            command_handler)
+        self.tanfolymok = CourseList(
+            self.get_module_number_by_name("Tanfolyamok"),
+            self.system_id,
+            self.api_key,
+            command_handler)
+        self.today = today
 
     @stacktrace
     def register_new_applicants(self):
@@ -217,9 +227,7 @@ class CrmData:
                     
                     Az adatok korrigálása után a rendszer automatiksuan megteszi a szokásos lépéseket, így ne küldj manuálisan INFO levelet, és ne változtasd meg a jelentkező státásuzát, mert az elronthatja a folyamatot!
                     """.format(student_data["MelyikTanfolyamErdekli"]),
-                    (datetime.datetime.now() + datetime.timedelta(days=3)).__str__(),
-                    #crm_data.get_userid_by_name("Rudolf Dániel")
-                    )
+                    (self.today + datetime.timedelta(days=3)).__str__())
 
     @stacktrace
     def clean_info_level_kiment(self):
@@ -249,19 +257,18 @@ class CrmData:
             trace("STUDENT ["+student+"](["+student_data["Name"]+"]) HAS NOT FINALIZED")
 
             deadline = datetime.datetime.strptime(student_data["VeglegesitesiHatarido"], "%Y-%m-%d %H:%M:%S")
-            today = datetime.datetime.now()
 
-            trace("TODAY: {}, DEADLINE: {}".format(today, deadline))
+            trace("TODAY: {}, DEADLINE: {}".format(self.today, deadline))
 
-            if today >= deadline + datetime.timedelta(days=-1):
+            if self.today >= deadline + datetime.timedelta(days=-1):
                 trace("In first if")
                 levelkuldesek = add_element_to_commasep_list(levelkuldesek, "Egy napod van jelentkezni")
 
-            if today >= deadline:
+            if self.today >= deadline:
                 trace("In second if")
                 levelkuldesek = add_element_to_commasep_list(levelkuldesek, "Ma kell jelentkezni")
 
-            if today >= deadline + datetime.timedelta(days=+1):
+            if self.today >= deadline + datetime.timedelta(days=+1):
                 trace("In third if")
                 levelkuldesek = add_element_to_commasep_list(levelkuldesek, "Toroltunk")
                 update_data["StatusId"] = self.jelentkezok.get_status_number_by_name("Nem jelzett vissza")
@@ -348,18 +355,16 @@ class CrmData:
 
             pretty_print(course_data)
 
-            today = datetime.datetime.now()
-
             update_data = {}
 
             try:
-                if today >= datetime.datetime.strptime(course_data["ElsoAlkalom"], "%Y-%m-%d %H:%M:%S"):
+                if self.today >= datetime.datetime.strptime(course_data["ElsoAlkalom"], "%Y-%m-%d %H:%M:%S"):
                     trace("Set: ElsoAlkalom")
                     update_data["StatusId"] = self.tanfolymok.get_status_number_by_name("Folyamatban")
-                if today >= datetime.datetime.strptime(course_data["N10Alkalom"], "%Y-%m-%d %H:%M:%S"):
+                if self.today >= datetime.datetime.strptime(course_data["N10Alkalom"], "%Y-%m-%d %H:%M:%S"):
                     trace("Set: N10Alkalom")
                     update_data["StatusId"] = self.tanfolymok.get_status_number_by_name("Frissen végzett")
-                if today >= datetime.datetime.strptime(course_data["N10Alkalom"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=35):
+                if self.today >= datetime.datetime.strptime(course_data["N10Alkalom"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=35):
                     trace("Set: N10Alkalom + 35 nap")
                     update_data["StatusId"] = self.tanfolymok.get_status_number_by_name("Befejezett")
             except:
@@ -392,8 +397,6 @@ class CrmData:
         trace("STUDENT LIST: ")
         pretty_print(student_list)
 
-        today = datetime.datetime.now()
-
         for student in student_list:
             student_data = self.get_project(student)
 
@@ -401,72 +404,71 @@ class CrmData:
 
             levelkuldesek = student_data["Levelkuldesek"]
             levelkuldesek_old = levelkuldesek
-            today = datetime.datetime.now()
 
-            if today >= datetime.datetime.strptime(student_data["N1Alkalom"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N1Alkalom"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N1Alkalom, NOW: {}")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "1. alkalom - kezdő")
                 elif (student_data["TanfolyamTipusa2"] == "Haladó programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "1. alkalom - haladó")
 
-            if today >= datetime.datetime.strptime(student_data["N2Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N2Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N2Alkalom2")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "2. alkalom - kezdő")
                 elif (student_data["TanfolyamTipusa2"] == "Haladó programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "2. alkalom - haladó")
 
-            if today >= datetime.datetime.strptime(student_data["N3Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N3Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N3Alkalom2")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "3. alkalom - kezdő")
                 elif (student_data["TanfolyamTipusa2"] == "Haladó programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "3. alkalom - haladó")
 
-            if today >= datetime.datetime.strptime(student_data["N4Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N4Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N4Alkalom2")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "4. alkalom - kezdő")
                 elif (student_data["TanfolyamTipusa2"] == "Haladó programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "4. alkalom - haladó")
 
-            if today >= datetime.datetime.strptime(student_data["N5Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N5Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N5Alkalom2")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "5. alkalom - kezdő")
                 elif (student_data["TanfolyamTipusa2"] == "Haladó programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "5. alkalom - haladó")
 
-            if today >= datetime.datetime.strptime(student_data["N6Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N6Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N6Alkalom2")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "6. alkalom - kezdő")
                 elif (student_data["TanfolyamTipusa2"] == "Haladó programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "6. alkalom - haladó")
 
-            if today >= datetime.datetime.strptime(student_data["N7Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N7Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N7Alkalom2")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "7. alkalom - kezdő")
                 elif (student_data["TanfolyamTipusa2"] == "Haladó programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "7. alkalom - haladó")
 
-            if today >= datetime.datetime.strptime(student_data["N8Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N8Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N8Alkalom2")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "8. alkalom - kezdő")
                 elif (student_data["TanfolyamTipusa2"] == "Haladó programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "8. alkalom - haladó")
 
-            if today >= datetime.datetime.strptime(student_data["N9Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N9Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N9Alkalom2")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "9. alkalom - kezdő")
                 elif (student_data["TanfolyamTipusa2"] == "Haladó programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "9. alkalom - haladó")
 
-            if today >= datetime.datetime.strptime(student_data["N10Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
+            if self.today >= datetime.datetime.strptime(student_data["N10Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-3):
                 trace("Set: N10Alkalom2")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "10. alkalom - kezdő")
@@ -475,7 +477,7 @@ class CrmData:
 
 
 
-            if today >= datetime.datetime.strptime(student_data["N10Alkalom2"], "%Y-%m-%d %H:%M:%S"):
+            if self.today >= datetime.datetime.strptime(student_data["N10Alkalom2"], "%Y-%m-%d %H:%M:%S"):
                 trace("Set: N10Alkalom2 + 1 nap")
                 if (student_data["TanfolyamTipusa2"] == "Kezdő programozó tanfolyam"):
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "Útravaló")
@@ -483,7 +485,7 @@ class CrmData:
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "Útravaló - haladó")
 
 
-            if today >= datetime.datetime.strptime(student_data["N10Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=1):
+            if self.today >= datetime.datetime.strptime(student_data["N10Alkalom2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=1):
                 trace("Set: N10Alkalom2 + 2 nap")
                 update_data["StatusId"] = self.jelentkezok.get_status_number_by_name("Elvégezte")
                 if self.ok_for_certification(student_data):
@@ -494,15 +496,15 @@ class CrmData:
                         levelkuldesek = add_element_to_commasep_list(levelkuldesek, "Oklevél - haladó")
 
             if student_data["N2SzunetOpcionalis2"] != "":
-                if today >= datetime.datetime.strptime(student_data["N2SzunetOpcionalis2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-2):
+                if self.today >= datetime.datetime.strptime(student_data["N2SzunetOpcionalis2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-2):
                     trace("Set: N2SzunetOpcionalis2")
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "1. szünet")
             if student_data["N2SzunetOpcionalis3"] != "":
-                if today >= datetime.datetime.strptime(student_data["N2SzunetOpcionalis3"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-2):
+                if self.today >= datetime.datetime.strptime(student_data["N2SzunetOpcionalis3"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-2):
                     trace("Set: N2SzunetOpcionalis3")
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "2. szünet")
             if student_data["N3SzunetOpcionalis2"] != "":
-                if today >= datetime.datetime.strptime(student_data["N3SzunetOpcionalis2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-2):
+                if self.today >= datetime.datetime.strptime(student_data["N3SzunetOpcionalis2"], "%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=-2):
                     trace("Set: N3SzunetOpcionalis2")
                     levelkuldesek = add_element_to_commasep_list(levelkuldesek, "3. szünet")
 
@@ -682,25 +684,24 @@ class CrmData:
         """
         days_left_to_apply = 5
         starting_day = datetime.datetime.strptime(course_data["ElsoAlkalom"], "%Y-%m-%d %H:%M:%S")
-        today = datetime.datetime.now()
         free_spots = course_data["MaximalisLetszam"] - course_data["AktualisLetszam"]
         all_spots = course_data["MaximalisLetszam"]
         if all_spots == 0:
             all_spots = 1
 
-        if starting_day - today < datetime.timedelta(days = 7) or ((1.0*free_spots) / (1.0*all_spots)) < 0.3:
+        if starting_day - self.today < datetime.timedelta(days = 7) or ((1.0*free_spots) / (1.0*all_spots)) < 0.3:
             days_left_to_apply = 3
 
-        if starting_day - today < datetime.timedelta(days = 3) and free_spots <= 3:
+        if starting_day - self.today < datetime.timedelta(days = 3) and free_spots <= 3:
             days_left_to_apply = 1
 
-        deadline = today + datetime.timedelta(days=days_left_to_apply)
+        deadline = self.today + datetime.timedelta(days=days_left_to_apply)
 
         if deadline > starting_day:
             deadline = starting_day + datetime.timedelta(days=-1)
 
-        if deadline < today:
-            deadline = today + datetime.timedelta(days=1)
+        if deadline < self.today:
+            deadline = self.today + datetime.timedelta(days=1)
 
         return deadline.__str__()
 
--- test/minicrm_api_mock/commandhandlermock.py
+++ test/minicrm_api_mock/commandhandlermock.py
@@ -1,5 +1,5 @@
-from tracing import stacktrace, trace
-
+from tracing import stacktrace, trace, pretty_print
+import json
 
 class CommandHandlerMock:
 
@@ -11,7 +11,12 @@ class CommandHandlerMock:
     def get_json_array_for_command(self, command):
         print("COMMAND SENT TO ---MOCK--- API: {}".format(command))
         self.commands_got_in_parameters.append(command)
-        return self.read_file(self.return_values[command])
+        output = self.read_file(self.return_values[command])
+        trace("RAW RECEIVED: {}".format(output))
+        formatted_output = json.loads(output)
+        trace("ANSWER RECEIVED:")
+        pretty_print(formatted_output)
+        return formatted_output
 
     @stacktrace
     def set_return_value(self, command, filename):

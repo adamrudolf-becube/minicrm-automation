517c2bda183ea8a0baaade7fd5659548d3b7ba04
BeCube <jelentkezem@becube.hu>
2019-02-19 00:15:18 +0100

    Implement remaining tests in test_info_sent.py

--- test/test_info_sent.py
+++ test/test_info_sent.py
@@ -38,32 +38,69 @@ class TestInfoSent(unittest.TestCase):
     def set_participant_number_expectations(self):
         self.command_handler.expect_command('curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?StatusId=2753"', 'api_outputs/status_id_2753_one_course_open.json')
 
-        self.command_handler.expect_command('curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2037"', 'api_outputs/project_2037.json')
+        self.command_handler.expect_command('curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2037"', 'api_outputs/project_2037_2019-1_Q.json')
         self.command_handler.expect_command('curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?TanfolyamKodja=2019-1-Q"', 'api_outputs/tanfolyam_kodja_2019_1_Q.json')
         self.command_handler.expect_command('curl -s --user FakeUserName:FakeApiKey -XPUT "https://r3.minicrm.hu/Api/R3/Project/2037" -d \'{"AktualisLetszam":6}\'', 'api_outputs/xput_2037.json')
 
     def test_student_did_not_finalize_deadline_has_not_spent_but_within_24_hours_send_reminder(self):
-        """
-        Given:
-        When:
-        Then:
-        """
 
-        self.command_handler.expect_command('curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?StatusId=2781"', 'api_outputs/status_id_2781_one_student_info_sent.json')
+        self.crm_data.set_today(datetime.datetime(2019, 1, 22, 12, 0))
+
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?StatusId=2781"',
+            'api_outputs/status_id_2781_one_student_info_sent.json')
         self.set_participant_number_expectations()
-        self.command_handler.expect_command('curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2601"', "api_outputs/project_2601_fake_student.json")
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2601"',
+            "api_outputs/project_2601_fake_student.json")
+
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey -XPUT "https://r3.minicrm.hu/Api/R3/Project/2601" -d \'{"Levelkuldesek":"Kezd\u0151 INFO lev\u00e9l, Egy napod van jelentkezni"}\'',
+            'api_outputs/xput_2037.json')
+
         self.set_participant_number_expectations()
 
         self.crm_data.clean_info_level_kiment()
 
-    #def test_student_did_not_finalize_deadline_has_not_spent_but_within_24_hours_send_reminder(self):
-    #    pass
+    def test_student_did_not_finalize_deadline_has_spent_but_not_more_than_24_hours_ago_send_reminder_raise_task(self):
 
-    #def test_student_did_not_finalize_deadline_has_spent_but_not_more_than_24_hours_ago_send_reminder_raise_task(self):
-    #    pass
+        self.crm_data.set_today(datetime.datetime(2019, 1, 23, 12, 0))
 
-    #def test_student_did_not_finalize_deadline_has_spent_more_than_24_hours_ago_delete(self):
-    #    pass
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?StatusId=2781"',
+            'api_outputs/status_id_2781_one_student_info_sent.json')
+        self.set_participant_number_expectations()
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2601"',
+            "api_outputs/project_2601_fake_student.json")
+
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey -XPUT "https://r3.minicrm.hu/Api/R3/Project/2601" -d \'{"Levelkuldesek":"Kezd\u0151 INFO lev\u00e9l, Egy napod van jelentkezni, Ma kell jelentkezni"}\'',
+            'api_outputs/xput_2037.json')
+
+        self.set_participant_number_expectations()
+
+        self.crm_data.clean_info_level_kiment()
+
+    def test_student_did_not_finalize_deadline_has_spent_more_than_24_hours_ago_delete(self):
+
+        self.crm_data.set_today(datetime.datetime(2019, 1, 24, 12, 0))
+
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?StatusId=2781"',
+            'api_outputs/status_id_2781_one_student_info_sent.json')
+        self.set_participant_number_expectations()
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2601"',
+            "api_outputs/project_2601_fake_student.json")
+
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey -XPUT "https://r3.minicrm.hu/Api/R3/Project/2601" -d \'{"StatusId":"2782","Levelkuldesek":"Kezd\u0151 INFO lev\u00e9l, Egy napod van jelentkezni, Ma kell jelentkezni, Toroltunk"}\'',
+            'api_outputs/xput_2037.json')
+
+        self.set_participant_number_expectations()
+
+        self.crm_data.clean_info_level_kiment()
 
     def test_student_did_not_finalize_and_deadline_is_more_than_1_day_away_do_nothing(self):
 

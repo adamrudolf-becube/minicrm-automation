4f07e84d8cf1d1e7313930ffeffb7e9b80a38e91
BeCube <jelentkezem@becube.hu>
2019-02-19 00:45:41 +0100

    Implement remaining tests in test_handle_waiting_list

--- test/test_handle_waiting_list.py
+++ test/test_handle_waiting_list.py
@@ -109,20 +109,100 @@ class TestHandleWaitingList(unittest.TestCase):
         self.set_participant_number_expectations()
         self.command_handler.expect_command(
             'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
+            'api_outputs/project_2037_2019-1_Q_one_place_free.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey -XPUT "https://r3.minicrm.hu/Api/R3/Project/2601" -d \'{"StatusId":"2781","Levelkuldesek":"Kezd\u0151 INFO lev\u00e9l, Kezd\u0151 INFO lev\u00e9l, Felszabadult egy hely"}\'',
+            'api_outputs/xput_2037.json')
+        self.set_participant_number_expectations()
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
+            'api_outputs/project_2037_2019-1_Q_full.json'
+        )
+        self.set_participant_number_expectations()
+        self.crm_data.handle_waiting_list()
+
+    def test_there_are_multiple_students_on_waiting_list_and_there_are_two_free_places_put_both_students_to_info_sent(self):
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?StatusId=2750"',
+            'api_outputs/waiting_list_two_students_status_2750.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2790"',
+            'api_outputs/project_2601_fake_student.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2796"',
+            'api_outputs/project_later_fake_student.json')
+        self.set_participant_number_expectations()
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
+            'api_outputs/project_2037_2019-1_Q_one_place_free.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey -XPUT "https://r3.minicrm.hu/Api/R3/Project/2601" -d \'{"StatusId":"2781","Levelkuldesek":"Kezd\u0151 INFO lev\u00e9l, Kezd\u0151 INFO lev\u00e9l, Felszabadult egy hely"}\'',
+            'api_outputs/xput_2037.json')
+        self.set_participant_number_expectations()
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
             'api_outputs/project_2037_2019-1_Q_one_place_free.json'
+        )
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey -XPUT "https://r3.minicrm.hu/Api/R3/Project/2602" -d \'{"StatusId":"2781","Levelkuldesek":"Kezd\u0151 INFO lev\u00e9l, Kezd\u0151 INFO lev\u00e9l, Felszabadult egy hely"}\'',
+            'api_outputs/xput_2037.json')
+        self.set_participant_number_expectations()
+        self.crm_data.handle_waiting_list()
+
+    def test_there_are_5_students_on_the_waiting_list_and_there_are_two_free_places_put_the_earliest_two_to_info_sent(self):
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?StatusId=2750"',
+            'api_outputs/waiting_list_five_students_status_2750.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2798"',
+            'api_outputs/project_2601_fake_student.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2790"',
+            'api_outputs/project_later_fake_student.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2799"',
+            'api_outputs/project_later_fake_student.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2796"',
+            'api_outputs/project_fourth_fake_student.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/2797"',
+            'api_outputs/project_fifth_fake_student.json')
+
+        self.set_participant_number_expectations()
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
+            'api_outputs/project_2037_2019-1_Q_one_place_free.json')
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey -XPUT "https://r3.minicrm.hu/Api/R3/Project/2601" -d \'{"StatusId":"2781","Levelkuldesek":"Kezd\u0151 INFO lev\u00e9l, Kezd\u0151 INFO lev\u00e9l, Felszabadult egy hely"}\'',
+            'api_outputs/xput_2037.json')
+        self.set_participant_number_expectations()
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
+            'api_outputs/project_2037_2019-1_Q_one_place_free.json'
+        )
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey -XPUT "https://r3.minicrm.hu/Api/R3/Project/2602" -d \'{"StatusId":"2781","Levelkuldesek":"Kezd\u0151 INFO lev\u00e9l, Kezd\u0151 INFO lev\u00e9l, Felszabadult egy hely"}\'',
+            'api_outputs/xput_2037.json')
+
+        self.set_participant_number_expectations()
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
+            'api_outputs/project_2037_2019-1_Q_full.json'
         )
         self.set_participant_number_expectations()
         self.command_handler.expect_command(
             'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
             'api_outputs/project_2037_2019-1_Q_full.json'
         )
+        self.set_participant_number_expectations()
+        self.command_handler.expect_command(
+            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
+            'api_outputs/project_2037_2019-1_Q_full.json'
+        )
+
+        self.set_participant_number_expectations()
         self.crm_data.handle_waiting_list()
-
-    def test_there_are_multiple_students_on_waiting_list_and_there_are_two_free_places_put_both_students_to_info_sent(self):
-        pass
-
-    def test_there_are_5_students_on_the_waiting_list_and_there_are_two_free_places_put_the_earliest_two_to_info_sent(self):
-        pass
 
 if __name__ == '__main__':
     unittest.main()

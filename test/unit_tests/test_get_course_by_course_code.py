e33a0d6c2662b673847eecdc41961cd48946987e
BeCube <jelentkezem@becube.hu>
2019-04-12 08:26:28 +0200

    Modify test_get_course_by_course_code to use crm command factory

--- test/unit_tests/test_get_course_by_course_code.py
+++ test/unit_tests/test_get_course_by_course_code.py
@@ -7,13 +7,12 @@ class TestGetCourseByCourseCode(MiniCrmTestBase):
         wanted_course_code = "2019-1-Q"
 
         self.command_handler.expect_command(
-            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?TanfolyamBetujele={}"'. \
-                format(wanted_course_code),
+            self.crm_command_factory.get_course_list_by_course_code(wanted_course_code),
             'course_list_for_course_code'
         )
 
         self.command_handler.expect_command(
-            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project/1164"',
+            self.crm_command_factory.get_course(1164),
             'project_2037_2019-1_Q'
         )
         course_info = self.crm_data.get_course_by_course_code(wanted_course_code)

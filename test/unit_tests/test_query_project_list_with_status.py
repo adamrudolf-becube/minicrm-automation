34aaebfa9bebaa6d84808bb21e90d6ef62a10bbb
BeCube <jelentkezem@becube.hu>
2019-04-12 08:29:36 +0200

    Modify test_query_project_list_with_status to use crm command factory

--- test/unit_tests/test_query_project_list_with_status.py
+++ test/unit_tests/test_query_project_list_with_status.py
@@ -4,12 +4,12 @@ from minicrmtestbase import MiniCrmTestBase
 class TestQueryProjectListWithStatus(MiniCrmTestBase):
     def test_query_project_list_with_status_returns_all_projects_even_if_thereare_more_pages(self):
         self.command_handler.expect_command(
-            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?StatusId=2749"',
+            self.crm_command_factory.get_project_list_for_status(2749),
             'student_list_105_active_page0'
         )
 
         self.command_handler.expect_command(
-            'curl -s --user FakeUserName:FakeApiKey "https://r3.minicrm.hu/Api/R3/Project?StatusId=2749&Page=1"',
+            self.crm_command_factory.get_project_list_for_status_page1(2749),
             'student_list_105_active_page1'
         )
         results = self.crm_data.get_student_list_with_status("Kurzus folyamatban")

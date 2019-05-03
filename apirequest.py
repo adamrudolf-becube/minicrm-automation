c296add7c9b731bd827e5a5df4d8471fafadec9c
BeCube <jelentkezem@becube.hu>
2019-05-03 08:14:54 +0200

    Create ApiRequest class

--- apirequest.py
+++ apirequest.py
@@ -1,6 +1,25 @@
+GET_METHOD = "GET"
+PUT_METHOD = "PUT"
+
+
 class ApiRequest:
     def __init__(self, url, method, slogan, payload={}):
-        self.url = url
-        self.method = method
-        self.slogan = slogan
-        self.payload = payload
+        self._url = url
+        if method != GET_METHOD and method != PUT_METHOD:
+            raise ValueError("Unsupported method type: {}".format(method))
+        else:
+            self._method = method
+        self._slogan = slogan
+        self._payload = payload
+
+    def get_url(self):
+        return self._url
+
+    def get_method(self):
+        return self._method
+
+    def get_slogan(self):
+        return self._slogan
+
+    def get_payload(self):
+        return self._payload

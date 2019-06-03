18934e1e03a1399da0572b3c38fd2324ab195cd7
BeCube <jelentkezem@becube.hu>
2019-06-03 08:31:24 +0200

    Add documentation to setcoursestates

--- functionalities/setcoursestates.py
+++ functionalities/setcoursestates.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-# MiniCRM automation
-# Copyright Adam Rudolf, 2018
-# BeCube programming school
+"""
+Updates the course stated based on their set states.
 
-from __future__ import print_function
+BeCube MiniCRM automation project.
+"""
+
+__author__ = "Adam Rudolf"
+__copyright__ = "Adam Rudolf, 2018"
 
 from minicrm.commonfunctions import merge_dicts, date_is_not_less_than
 from minicrm.tracing import stacktrace, trace, pretty_print
@@ -20,7 +23,19 @@ STATUS_ID_FIELD = "StatusId"
 @stacktrace
 def set_course_states(crm_facade):
     """
-    Loops through the courses and sets their statuses according to first and last date
+    Loops through the courses and sets their statuses according to first and last date.
+
+    - If current date is not less than course's 1st occasion, function sets the state to "In progress" ("Kurzus
+      folyamatban").
+    - If current date is not less than course's last occasion, function sets the state to "Recently finished" ("Frissen
+      vegzett"). This state if to make it possible to make certain actions (make statistics / calculate salaries)
+      monthly and the monthly script has to loop through the in progress and recently finished courses.
+    - If current date is not less than course's last occasion plus 35 days, function sets the state to "Finished"
+      ("Befejezett").
+
+    :param crm_facade: instance of the CrmFacade class this functionality will use to communicate with a MiniCRM system.
+
+    :return: None
     """
     open_courses = crm_facade.get_course_list_with_status(APPLICATION_OPEN_STATE)
     ongoing_courses = crm_facade.get_course_list_with_status(IN_PROGRESS_STATE)

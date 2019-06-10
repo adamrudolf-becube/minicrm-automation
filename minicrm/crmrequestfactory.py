b109f21e6d3ddaf5b55bdc07c978a7065d5a4f95
BeCube <jelentkezem@becube.hu>
2019-06-10 18:32:10 +0200

    Add doccomments to crmrequestfactory.py

--- minicrm/crmrequestfactory.py
+++ minicrm/crmrequestfactory.py
@@ -3,14 +3,38 @@ This library defines abstract functionalities and returns a string for them. Thi
 higher level logic independent from the concrete commands, but also to help phrase the exoectations in the tests
 """
 
+__author__ = "Adam Rudolf"
+__copyright__ = "Adam Rudolf, 2018"
+
 import json
 
 from apirequest import ApiRequest, GET_METHOD, PUT_METHOD
 
 _ = "WILDCARD"
+"""
+This is for testing purposes.
+
+Can be used in place of every payload. If a request is created with a payload like this, it indicates for the testing
+machinery that the test is expected with any payload. (Keeping in mind that the payload can have effect to the slogan
+as well.) 
+"""
 
 
 def get_schema_for_module_number(module_id):
+    """
+    Creates an ApiRequest instance for getting the schema of a MiniCRM module.
+
+    Examples can be: schema for students, schema for courses, or schema for locations. The schema is a JSON which
+    contains the fields, and their possible values.
+
+    Returns a GET request.
+
+    :param module_id: ID number of the requested module.
+    :type module_id: int
+
+    :return: the encapsuladed API request for getting the schema for the given module.
+    :rtype: ApiRequest
+    """
     return ApiRequest(
         "https://r3.minicrm.hu/Api/R3/Schema/Project/{}".format(module_id),
         GET_METHOD,
@@ -19,6 +43,23 @@ def get_schema_for_module_number(module_id):
 
 
 def get_project_list_for_status(status_number):
+    """
+    Creates an ApiRequest instance for getting the list of projects for the given status, for example getting a list
+    of all courses in progress.
+
+    Expects a status number.
+
+    The API is paginated, this request will only return the first 100 projects.
+
+    Returns a GET request.
+
+    :param status_number: ID number of the requested status.
+    :type status_number: int
+
+    :return: the encapsuladed API request for
+    :rtype: ApiRequest
+    """
+
     return ApiRequest(
         "https://r3.minicrm.hu/Api/R3/Project?StatusId={}".format(status_number),
         GET_METHOD,
@@ -27,6 +68,23 @@ def get_project_list_for_status(status_number):
 
 
 def get_project_list_for_status_page1(status_number):
+    """
+    Creates an ApiRequest instance for getting the second page (page 1) of the list of projects for the given status,
+    for example getting a list of all courses in progress.
+
+    Expects a status number.
+
+    The API is paginated, this request will only return the second 100 projects.
+
+    Returns a GET request.
+
+    :param status_number: ID number of the requested status.
+    :type status_number: int
+
+    :return: the encapsuladed API request for
+    :rtype: ApiRequest
+    """
+
     return ApiRequest(
         "https://r3.minicrm.hu/Api/R3/Project?StatusId={}&Page=1".format(status_number),
         GET_METHOD,
@@ -35,18 +93,79 @@ def get_project_list_for_status_page1(status_number):
 
 
 def get_student(student_id):
+    """
+    Creates an ApiRequest instance for getting the full data of a specific student, based on their ID number.
+
+    Note: this request will be the same as getting the data for any projects. Different functions have been created
+    though to improve readability.
+
+    Returns a GET request.
+
+    :param student_id: ID number of the requested student.
+    :type student_id: int
+
+    :return: the encapsuladed API request for getting the fll data of a specific student.
+    :rtype: ApiRequest
+    """
+
     return get_project(student_id)
 
 
 def get_location(location_id):
+    """
+    Creates an ApiRequest instance for getting the full data of a specific location, based on it's ID number.
+
+    Note: this request will be the same as getting the data for any projects. Different functions have been created
+    though to improve readability.
+
+    Returns a GET request.
+
+    :param location_id: ID number of the requested location.
+    :type location_id: int
+
+    :return: the encapsuladed API request for getting the fll data of a specific location.
+    :rtype: ApiRequest
+    """
+
     return get_project(location_id)
 
 
 def get_course(course_id):
+    """
+    Creates an ApiRequest instance for getting the full data of a specific course, based on it's ID number.
+
+    Note: this request will be the same as getting the data for any projects. Different functions have been created
+    though to improve readability.
+
+    Returns a GET request.
+
+    :param course_id: ID number of the requested course.
+    :type course_id: int
+
+    :return: the encapsuladed API request for getting the fll data of a specific course.
+    :rtype: ApiRequest
+    """
+
     return get_project(course_id)
 
 
 def get_course_list_by_course_code(course_code):
+    """
+    Creates an ApiRequest instance for getting a list of courses based on their course codes.
+
+    Note: in our use cases the course code should be unique, this is not enforced by the MiniCRM system, and for the
+    system it is only a regular text field. That is why the system is only able to return a list of courses for a given
+    course code, although (if used well) this list will only contain one course.
+
+    Returns a GET request.
+
+    :param course_code: code of the requested course, for example "2019-6-D"
+    :type course_code: str
+
+    :return: the encapsuladed API request for getting a list of the courses which have the given course code.
+    :rtype: ApiRequest
+    """
+
     return ApiRequest(
         "https://r3.minicrm.hu/Api/R3/Project?TanfolyamBetujele={}".format(course_code),
         GET_METHOD,
@@ -55,6 +174,22 @@ def get_course_list_by_course_code(course_code):
 
 
 def get_location_list_by_location_name(location_name):
+    """
+    Creates an ApiRequest instance for getting a list of locations based on their names.
+
+    Note: in our use cases the location name should be unique, this is not enforced by the MiniCRM system, and for the
+    system it is only a regular text field. That is why the system is only able to return a list of locations for a
+    given location name, although (if used well) this list will only contain one location.
+
+    Returns a GET request.
+
+    :param location_name: name of the requested location, for example "Astoria"
+    :type location_name: str
+
+    :return: the encapsuladed API request for getting a list of the locations which have the given name.
+    :rtype: ApiRequest
+    """
+
     return ApiRequest(
         "https://r3.minicrm.hu/Api/R3/Project?EgyediAzonosito={}".format(location_name),
         GET_METHOD,
@@ -63,6 +198,18 @@ def get_location_list_by_location_name(location_name):
 
 
 def get_project(project_id):
+    """
+    Creates an ApiRequest instance for getting the full data of any project (e.g. student, course, location).
+
+    Returns a GET request.
+
+    :param project_id: ID number of the requested project
+    :type project_id: int
+
+    :return: the encapsuladed API request for getting the full data of a project
+    :rtype: ApiRequest
+    """
+
     return ApiRequest(
         "https://r3.minicrm.hu/Api/R3/Project/{}".format(project_id),
         GET_METHOD,
@@ -71,6 +218,29 @@ def get_project(project_id):
 
 
 def set_project_data(project_id, data_json):
+    """
+    Creates an ApiRequest instance for modifying the data of an existing project (e.g. student, course or location).
+
+    Returns a PUT request.
+
+    Example usage:
+
+    This is how you create a request to set student 1234 to waiting lis status:
+
+    .. code-block:: python
+
+      request = set_project_data(1234, {"StatusId": 2781})
+
+    :param project_id: ID number of the project to be modified.
+    :type project_id: int
+
+    :param data_json: the dictionary of key-value pairs to be modified. You don't need to give the unchanged keys.
+    :type data_json: dict
+
+    :return: the encapsuladed API request for modifying the data of an existing project.
+    :rtype: ApiRequest
+    """
+
     if data_json == _:
         return ApiRequest(
             "https://r3.minicrm.hu/Api/R3/Project/{}".format(project_id),
@@ -88,6 +258,20 @@ def set_project_data(project_id, data_json):
 
 
 def get_student_list_by_course_code(course_code):
+    """
+    Creates an ApiRequest instance for getting a list of students based on their course codes.
+
+    This returns a list of students attending the same course.
+
+    Returns a GET request.
+
+    :param course_code: code of the requested course, for example "2019-6-D"
+    :type course_code: str
+
+    :return: the encapsuladed API request for getting a list of the students which have the given course code.
+    :rtype: ApiRequest
+    """
+
     return ApiRequest(
         "https://r3.minicrm.hu/Api/R3/Project?TanfolyamKodja={}".format(course_code),
         GET_METHOD,
@@ -96,6 +280,17 @@ def get_student_list_by_course_code(course_code):
 
 
 def get_modul_dictionary():
+    """
+    Creates an ApiRequest instance for getting the list (dictionary) of modules existing in the MiniCRM system.
+
+    The response is a dicionary where the keys are the ID numbers of the modules, and the values are the names of them.
+
+    Returns a GET request.
+
+    :return: the encapsuladed API request for¨getting the dictionary of the modules created in the MiniCRM system.
+    :rtype: ApiRequest
+    """
+
     return ApiRequest(
         "https://r3.minicrm.hu/Api/R3/Category",
         GET_METHOD,
@@ -109,7 +304,27 @@ def raise_task(
         deadline,
         userid=""):
     """
-    Creates a new task in teh CRM ssytem with the given details
+    Creates an ApiRequest instance for creating a new task in the MiniCRM system with the given details.
+
+    In MiniCRM you can create a task on every project (student, course, location), and every task has a deadline, a
+    text (also called comment) and a responsible, which is one of the users in MiniCRM.
+
+    :param project_id: ID number of the project (student/course/location) you would like to raise the task on. The task
+                       will be visible on the page of that project.
+    :type project_id: int
+
+    :param comment: the text you would like to show as the body of the task. Usually contains information what needs to
+                    be done and how.
+    :type comment: str
+
+    :param deadline: deadline of the task. Has to be in the "%Y-%m-%d %H:%M:%S" format.
+    :type deadline: str
+
+    :param user_id: MiniCRM user who is responsible (assignee) for the task
+    :type user_id: int
+
+    :return: the encapsuladed API request for
+    :rtype: ApiRequest
     """
 
     if project_id == _:

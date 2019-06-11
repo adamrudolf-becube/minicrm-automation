2e926b021295124bb4bbffc80716daf71909b59a
BeCube <jelentkezem@becube.hu>
2019-06-11 06:57:38 +0200

    Add docstrings to expectation.py

--- requesthandlermock/__init__.py
+++ requesthandlermock/__init__.py
@@ -1,1 +1,8 @@
-"""Documentation of requesthandlemock package"""
+"""
+This package provides a mock for the RequestHandler class to mimic a REST API connection for testing purposes.
+
+Contains every needed asset (supporting classes, predefined responses) which are needed for the mock to work well.
+"""
+
+__author__ = "Adam Rudolf"
+__copyright__ = "Adam Rudolf, 2018"
--- requesthandlermock/expectation.py
+++ requesthandlermock/expectation.py
@@ -1,4 +1,20 @@
+__author__ = "Adam Rudolf"
+__copyright__ = "Adam Rudolf, 2018"
+
+
 class Expectation:
     def __init__(self, expected_request, response):
+        """
+        Encapsulates an expectation in the test. The expectation contains the expected ApiRequest and a response to that
+        request as a Python dictionary.
+
+        :param expected_request: API request expected by the testcase
+        :type: ApiRequest
+
+        :param response: if the expected request is sent to a CommandHandlerMock instance, this is returned as a
+                         response.
+        :type response: dict
+        """
+
         self.request = expected_request
         self.response = response

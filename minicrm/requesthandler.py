a129db187f2d2b2e511561e790c3641fe3d4fc5d
BeCube <jelentkezem@becube.hu>
2019-06-11 06:28:01 +0200

    Add doccomments to requesthandler.py

--- minicrm/requesthandler.py
+++ minicrm/requesthandler.py
@@ -1,3 +1,10 @@
+"""
+Contains a class to maintain the connection with the remote server.
+"""
+
+__author__ = "Adam Rudolf"
+__copyright__ = "Adam Rudolf, 2018"
+
 import json
 
 import requests
@@ -9,13 +16,31 @@ from tracing import stacktrace, trace, pretty_print
 class RequestHandler:
 
     def __init__(self, username, api_key):
+        """
+        This class encapsulates a connection towards the MiniCRM server through it's REST API.
+
+        It is responsible to send the requests and receive and parse the answers.
+
+        :param username: username (system ID) for the API
+        :type username: str
+
+        :param api_key: API key
+        :type api_key: str
+        """
+
         self._username = username
         self._api_key = api_key
 
     @stacktrace
     def fetch(self, request):
         """
-        Sends the API request to the CRM system, and returns the formatted JSON array.
+        Sends the API request to the CRM system, and returns the formatted JSON array in the for of a Python dict.
+
+        :param request: the encapsulated API request
+        :type request: ApiRequest
+
+        :return: the JSON answer as Python dictionary.
+        :rtype: dict
         """
         trace("COMMAND SENT TO API: {}, URL: {}".format(request.get_slogan(), request.get_url()))
 
